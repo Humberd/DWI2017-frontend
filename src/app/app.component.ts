02487ab89a28513c868b57584072a86ccc1c10c9
Humberd <sawik11@wp.pl>
2017-06-07 19:08:54 +0200

    Added variable synchronization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  myTitle: string = "Hello From Component!!!";
+
+  constructor() {
+    setTimeout(() => {
+      this.myTitle = "I've changed in setTimeout..."
+    }, 2000)
+  }
 }
